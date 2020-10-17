4658943144195efc758938b1b04f8e3c75fa6334
Katalin Keszthelyi <kkeszthe@gmail.com>
2020-10-17 03:17:36 +0200

    fix: incorrect url

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,6 +1,6 @@
 export const env = {
   BACKEND_URL:
     process.env.NODE_ENV === 'production'
-      ? 'https://giggle-tribes.herokuapp.com'
+      ? 'https://giggle-greenbay.herokuapp.com/'
       : 'http://localhost:5000',
 };
